e6cf5ed9eefef411903f6450d2bf6ac5de8620f8
angelicawillis <angelica.willis94@gmail.com>
2016-10-30 14:40:22 -0400

    step 1

--- CODE2040.py
+++ CODE2040.py
@@ -1,1 +1,10 @@
+import requests
+import json
 
+
+token = '161fdc48632480b96fa68fe9abf8c8cd'
+github = 'ttps://github.com/AngieW/Code2040'
+
+payloadd = {'token': token, 'github': github}
+
+request = requests.post('http://challenge.code2040.org/api/register', params=payloadd)
